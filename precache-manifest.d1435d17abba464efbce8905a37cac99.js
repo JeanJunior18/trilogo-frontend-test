15aa5427dcf80d8e5c5dbdb8c9170859a1ff4d11
Jean Junior <jclgjunior@gmail.com>
2020-09-30 11:49:20 -0300

    Updates

---
+++ precache-manifest.d1435d17abba464efbce8905a37cac99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b25f42e2b9d0b7592c6392252416aec",
+    "url": "/trilogo-frontend-test/index.html"
+  },
+  {
+    "revision": "26e26216d3816d76f5fe",
+    "url": "/trilogo-frontend-test/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "be44554c6c20dca01f0e",
+    "url": "/trilogo-frontend-test/static/css/main.2b567794.chunk.css"
+  },
+  {
+    "revision": "26e26216d3816d76f5fe",
+    "url": "/trilogo-frontend-test/static/js/2.7ffafe7b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/trilogo-frontend-test/static/js/2.7ffafe7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be44554c6c20dca01f0e",
+    "url": "/trilogo-frontend-test/static/js/main.8661e444.chunk.js"
+  },
+  {
+    "revision": "15fcecc26a8f19c42a36",
+    "url": "/trilogo-frontend-test/static/js/runtime-main.b88fd73e.js"
+  }
+]);
